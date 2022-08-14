a5dceec75999472046a3629e64f1e6eac46f8d96
EvgeniyYaloza <111225530+EvgeniyYaloza@users.noreply.github.com>
2022-08-14 14:58:53 +0300

    Update main.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,5 @@
-import LoginModal from "https://github.com/EvgeniyYaloza/EvgeniyYaloza.github.io/blob/main/src/scripts/classes/LoginModal.js";
-import {toggleBtn} from "https://github.com/EvgeniyYaloza/EvgeniyYaloza.github.io/blob/main/src/scripts/functions/toggleBtn.js";
+import LoginModal from "EvgeniyYaloza.github.io/src/scripts/classes/LoginModal.js";
+import {toggleBtn} from "EvgeniyYaloza.github.io/src/scripts/functions/toggleBtn.js";
 
 const btnLogin = document.querySelector('.header__btn-login');
 
